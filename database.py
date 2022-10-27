04d1a9682e4d1d6036a36c6487ac78cd007299b6
Sze Huang <Huangls@live.com>
2022-10-27 17:29:49 +0800

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "mysql://roy:roy@localhost/metlife"
 DATABASE_URL = "mysql+mysqlconnector://metlifeadmin:Simple123@metlife.mysql.database.azure.com:3306/metlife"
 engine = create_engine(DATABASE_URL)
 
